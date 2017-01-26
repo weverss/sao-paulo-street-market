015925fba98e5f64602be114d4b5c7d7d3b133c1
Wevers Santos <weverss@gmail.com>
2017-01-26 18:00:33 -0200

    Add migration that creates street markets table

---
+++ database/migrations/2017_01_26_175951_create_street_markets_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStreetMarketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('street_markets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('registration_code', 6);
+            $table->bigInteger('census_tract');
+            $table->bigInteger('census_tract_group');
+            $table->string('street_market_name');
+            $table->string('street_name');
+            $table->string('street_number');
+            $table->string('neighborhood');
+            $table->string('landmark');
+            $table->integer('latitude');
+            $table->integer('longitude');
+            $table->tinyInteger('district_code');
+            $table->string('district_name');
+            $table->tinyInteger('district council_code');
+            $table->string('district council_name');
+            $table->string('five_area_region');
+            $table->string('eight_area_region');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('street_markets');
+    }
+}
