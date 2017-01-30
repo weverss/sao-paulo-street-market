3ec611603b8ee796a132320d05ac4963b1d6d6d5
Wevers Santos <weverss@gmail.com>
2017-01-30 19:45:19 -0200

    Remove output testing due to datetime params. Add exit code testing

--- tests/Unit/ImportStreetMarketsTest.php
+++ tests/Unit/ImportStreetMarketsTest.php
@@ -12,26 +12,8 @@ class ImportStreetMarketsTest extends TestCase
 {
     public function testHandle()
     {
-        Artisan::call('import:street-markets', ['filepath' => base_path('tests/TestFile.csv')]);
+        $exitCode = Artisan::call('import:street-markets', ['filepath' => base_path('tests/TestFile.csv')]);
 
-    $expected = <<<EOT
-Importando CSV
-Feira 1 de 10 importada: 4041-0 Vila Formosa
-Feira 2 de 10 importada: 4045-2 Praca Santa Helena
-Feira 3 de 10 importada: 4003-7 Concordia
-Feira 4 de 10 importada: 3048-1 Vila Nova Granada
-Feira 5 de 10 importada: 7210-9 Parque Savoy City
-Feira 6 de 10 importada: 1087-1 Mar Paulista
-Feira 7 de 10 importada: 1040-5 Cruz Das Almas
-Feira 8 de 10 importada: 7222-2 Lajeado
-Feira 9 de 10 importada: 3008-2 Alto Das Perdizes
-Feira 10 de 10 importada: 4038-0 Maria Carlota
-Concluído.
-
-EOT;
-
-        $actual = Artisan::output();
-
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals(0, $exitCode);
     }
 }
