69357f1c5e7f53dc376201054b3a3c257bbdb84f
Wevers Santos <weverss@gmail.com>
2017-01-30 12:12:20 -0200

    Add guarded and cast attributes

--- app/StreetMarket.php
+++ app/StreetMarket.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class StreetMarket extends Model
 {
-    //
+    protected $guarded = [
+        'id',
+        'registration_code',
+    ];
+
+    public function getStreetNumberAttribute()
+    {
+        $streetNumber = $this->attributes['street_number'];
+
+        if(is_numeric($streetNumber)) {
+            return intval($streetNumber);
+        }
+
+        return $streetNumber;
+    }
 }
